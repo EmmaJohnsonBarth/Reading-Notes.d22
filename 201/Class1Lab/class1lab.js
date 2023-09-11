3806e96e35d7492ec9d6f7af3044bd0f7f49f687
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-09-10 20:33:26 -0500

    corrected link to js maybe

--- 201/Class1Lab/class1lab.js
+++ 201/Class1Lab/class1lab.js
@@ -1,7 +1,6 @@
 
 
-const name = prompt("What is your name?");
-const dob = prompt("what year were you born?");
-const age = 2023 - dob;
-const guess = prompt(`You are ${age}, correct?`);
-const ask = prompt("Coding is cool, right?");
+let name = prompt("What is your name?");
+let quest = prompt("What is your quest?");
+let favColor = prompt("What is your favorite color?");
+let birdQ = prompt("African or European swallow?");
