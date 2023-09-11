6b6337bf260673a646a90f8d16988997c050e2cd
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-09-10 20:48:16 -0500

    added console.logs

--- 201/Class1Lab/class1lab.js
+++ 201/Class1Lab/class1lab.js
@@ -1,6 +1,17 @@
+"use strict"
 
+console.log("check");
 
-let name = prompt("What is your name?");
-let quest = prompt("What is your quest?");
-let favColor = prompt("What is your favorite color?");
-let birdQ = prompt("African or European swallow?");
+let nameQ = prompt("What is your name?");
+console.log("name: ", nameQ);
+
+let questQ = prompt("What is your quest?");
+console.log("quest: ", questQ);
+
+let favColorQ = prompt("What is your favorite color?");
+console.log("color: ", favColorQ)
+
+let birdQ = prompt("African or European swallow?");\
+console.log("bird: ", birdQ)
+
+let answers = alert(`So, you are ${nameQ}, on a quest for ${questQ}, your favorite color is ${favColorQ}, and you prefer the ${birdQ} swallow, correct?`)
