9217941e243a84df74b5861db4cc7ed3ea078863
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-09-13 15:41:48 -0500

    worked on q7

--- 201/AboutMe/js/code.js
+++ 201/AboutMe/js/code.js
@@ -117,11 +117,5 @@ function questionSeven() {
         alert('Guess again! You\'ve taken ' + attemptsQ7 + 'out of 6 attempts!');
         questionSeven();
     }
-}
+};
 
-
-// Add a 7th question that has multiple possible correct answers that are stored in an array.
-// Give the user 6 attempts to guess the correct answer.
-// The guesses will end once the user guesses a correct answer or they run out of attempts.
-// Display all the possible correct answers to the user.
-// Consider using a loop of some sort for this question.
